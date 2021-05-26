e59b45d0fb5e7273b96ca6be227927abeb59bf87
onogawa <nananananana21@gmail.com>
2021-05-26 23:40:00 +0000

    first commit

--- practice.php
+++ practice.php
@@ -1,3 +1,16 @@
 <?php
-$array_month=["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
-echo$array_month[7];
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[7];
+echo "\n";
+$hello = 'Hello';
+$name = ' NaNa';
+$world = "'s world!";
+$value = $hello;
+$value .= $name;
+$value .= $world;
+echo $value;
+echo "\n";
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
+echo "\n";
