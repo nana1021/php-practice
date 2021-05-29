df3d3d13b1554bf3b78d7fe2f046c283b94bf533
onogawa <nananananana21@gmail.com>
2021-05-29 16:21:06 +0000

    php03課題

--- practice03.php
+++ practice03.php
@@ -1,17 +1,36 @@
 <?php
-$calendar2018  = [
-  "January" => "1月",
-  "February" => "2月",
-  "March" => "3月",
-  "April" => "4月",
-  "May" => "5月",
-  "June" => "6月",
-  "July" => "7月",
-  "August" => "8月",
-  "September" => "9月",
-  "October" => "10月",
-  "November" => "11月",
-  "December" => "12月"
-];
-echo $calendar2018["December"];
+$name = "オノガワ";
+if ($name = "オノガワ") {
+    echo "私はオノガワです";
+} else {
+echo "あなたの名前ではありません";
+}
 echo "\n";
+
+$total = 0;
+echo $total;
+for ($i = 0; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+echo "\n";
+
+$fruits = array("lemon","apple","grape","lime","peach");
+foreach($fruits as $fruits){
+    echo "要素は" .$fruits;
+    echo "\n";
+}
+
+//* for文の始めの値を定義する /
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i < $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
